7f9dba90b17a5709bed69b59b0542c98e017ae12
Gennady Romanov <Relect@bk.ru>
2022-07-25 16:42:09 +0300

    first commit

---
+++ src/main/java/com/javainuse/SpringBootFileDownload.java
@@ -0,0 +1,13 @@
+package com.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFileDownload {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFileDownload.class, args);
+	}
+
+}
